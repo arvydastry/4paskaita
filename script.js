d6fb7846f9ed726ed1fe8a58723ecd46638bf760
arvydastry <arvydas.trybe@gmail.com>
2022-05-03 20:47:11 +0300

    4 paskaita

--- script.js
+++ script.js
@@ -1,1 +1,123 @@
-console.log(Arvydas)
+// console.log("for ciklai");
+
+// Naudojamas FOR dazniausiai!
+
+// i++ prideda 1
+// i + i + 1;
+
+// i-- atima 1
+// i = i -1
+
+// += x += y   x = x + y
+// -=  x-= y   x = x - y
+
+
+
+// kintamieji turi buti - i,j,k
+//   [inicializavimas] ; [salyga] ; [israiska]
+// for (let i = 0; i < 10; i++) {
+//     console.log(`${i + 1}. Arvydas)`;
+// }
+
+
+
+// let i =10;
+// while (i > 0) {
+//     console.log(i);
+//     i--;
+// }
+
+
+// for (let i = 10; i > 0; 1--) {
+//     console.log(i);
+// }
+
+// i = 10; 10 > 0; 10
+// i = 9; 9> 0; 9
+// ....
+// i = 0;  0 > 0; 0
+
+
+// for (let i=0; i < 10; i++) {
+//     console.log(i);
+// }
+
+
+
+// for (let i=0; i < 10; i++) {
+//     console.log(i);
+// }
+// // i = 0; 0 < 10
+
+
+// for (let i=0; i < 10; i++) {
+//     console.log(i);
+// }
+
+
+// const dogName = "Rikis";
+// const repeat = 5;
+// let result = "";
+
+// for (let i = 0; i < repeat; i++) {
+// result += `${dogName}`;
+// }
+
+// a = a + b
+// "" = "" + "Rikis"
+// "Rikis" = "Rikis" + "Rikis"
+// "RikisRikis" = "RikisRikis" + "Rikis"
+// "RikisRikisRikis" = "RikisRikisRikis" + "Rikis"
+// "RikisRikisRikisRikis" = "RikisRikisRikisRikis" + "Rikis"
+
+
+// for (let i = 0; i < repeat; i++) {
+//     if (i === repeat - 1) {
+//     // paskutinis ciklas
+//     result += `${dogName}.`;
+//     } else {
+//     result += `${dogName}, `;
+//     }
+// }
+
+
+// uzduotis 1
+
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//       console.log(i);
+//     }
+//   }
+
+
+  // uzduotis 2
+
+// let x = 9;
+// while (x >= 1) {
+//     console.log("hello" + x);
+// x = x -1;
+// }
+
+
+// const hi = "hello";
+
+// for (let i = 9; i >= 1; i++) {
+//     if (i === repeat - 1) {
+//         console.log(i);
+//     result += `${i}`;
+//     }
+// }
+
+
+
+// uzduotis 3
+
+
+for (let i = 0; i < repeat; i++) {
+    if (i === repeat - 1) {
+    // paskutinis ciklas
+    result += `${dogName}.`;
+    } else {
+    result += `${dogName}, `;
+    }
+}
